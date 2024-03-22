79e15b3d1cbb9d303e5d8aef07c5cd15a5c98c06
Maxim <ttoptpet@mail.ru>
2024-03-22 14:48:47 +0300

    #161 BUG: не давать бронировать если не верифицирован

--- src/pages/TourPage/TourRouting/TourRouting.tsx
+++ src/pages/TourPage/TourRouting/TourRouting.tsx
@@ -1,13 +1,15 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { tourStepsMap } from "../TourPage";
 import { useNavigate } from "react-router-dom";
-import { Stack, Button } from "@mui/material";
+import { Stack, Button, Typography } from "@mui/material";
 import { useCookies } from "react-cookie";
 import { LOGGINED, ROLE } from "../../../config/types";
 import { useDispatch } from "react-redux";
 import { setModalActive } from "../../../redux/Modal/ModalReducer";
 import { ITourBookingData } from "../../../models/tourModels/ITourBookingData";
 import { booking } from "../../../API/touristAPI/booking";
+import { getUserInfo } from "../../../API/commonAPI";
+import { redColor } from "../../../config/MUI/color/color";
 
 interface TourRoutingProps {
   page: tourStepsMap;
@@ -23,6 +25,7 @@ export const TourRouting: FC<TourRoutingProps> = ({
   const navigate = useNavigate();
 
   const [cookies, setCookies, removeCookies] = useCookies([LOGGINED, ROLE]);
+  const [isVerified, setIsVerified] = useState(true);
 
   const dispatch = useDispatch();
 
@@ -40,13 +43,19 @@ export const TourRouting: FC<TourRoutingProps> = ({
     );
   };
 
+  useEffect(() => {
+    getUserInfo((value) => {
+      setIsVerified(value.is_verified as boolean);
+    });
+  }, []);
+
   return (
     <>
       <Stack
         direction={"row"}
         justifyContent={"space-between"}
         alignItems={"center"}
-        sx={{ mb: "20px" }}
+        sx={{ mb: !isVerified ? "5px" : "20px" }}
       >
         <Button
           variant="textButton"
@@ -63,32 +72,46 @@ export const TourRouting: FC<TourRoutingProps> = ({
         >
           {"< "} Назад
         </Button>
-        {cookies.ROLE !== 2 &&
-          cookies.ROLE !== 3 &&
-          (page === 0 ? (
-            <Button
-              variant={"contained"}
-              sx={{ marginTop: 1 }}
-              onClick={() => {
-                cookies.LOGGINED
-                  ? setPage((page: tourStepsMap) =>
-                      page < 2 ? page + 1 : page
-                    )
-                  : dispatch(setModalActive("noLoginModal"));
-              }}
-            >
-              Забронировать
-            </Button>
-          ) : (
-            <Stack direction={"row"} gap={2} mt={"8px"}>
+        <Stack
+          display="flex"
+          direction={"column"}
+          gap={"10px"}
+          justifyContent={"flex-end"}
+          alignItems={"flex-end"}
+        >
+          {cookies.ROLE !== 2 &&
+            cookies.ROLE !== 3 &&
+            (page === 0 ? (
               <Button
-                onClick={() => handlerPurchaseClick()}
-                disabled={bookingData.touristsAmount === 0}
+                variant={"contained"}
+                sx={{ marginTop: 1 }}
+                disabled={!isVerified}
+                onClick={() => {
+                  cookies.LOGGINED
+                    ? setPage((page: tourStepsMap) =>
+                        page < 2 ? page + 1 : page
+                      )
+                    : dispatch(setModalActive("noLoginModal"));
+                }}
               >
-                Оплатить
+                Забронировать
               </Button>
-            </Stack>
-          ))}
+            ) : (
+              <Stack direction={"row"} gap={2} mt={"10px"}>
+                <Button
+                  onClick={() => handlerPurchaseClick()}
+                  disabled={bookingData.touristsAmount === 0}
+                >
+                  Оплатить
+                </Button>
+              </Stack>
+            ))}
+          {!isVerified && (
+            <Typography variant={"caption"} sx={{ color: redColor }}>
+              Для бронирования необходимо подтвердить почту
+            </Typography>
+          )}
+        </Stack>
       </Stack>
     </>
   );
